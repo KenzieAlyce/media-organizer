15d449b74cf0e3c46b5c073179ce9b43b7a30a31
Sarah Finck <sarahfinck95@gmail.com>
2021-05-10 21:33:25 -0500

    created db

---
+++ config/db/MediaSearch.Schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS media_search_db;
+CREATE database media_search_db;
+
+USE media_search_db;
+
+CREATE TABLE users
+(
+	id int NOT NULL AUTO_INCREMENT,
+	user_name varchar(255) NOT NULL,
+	user_email varchar(255) NOT NULL,
+	user_password varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE media 
+(
+	id int NOT NULL AUTO_INCREMENT,
+	media_name varchar(255) NOT NULL,
+	media_type varchar(255) NOT NULL,
+	media_artist varchar(255) NOT NULL,
+	media_location varchar(255) NOT NULL,
+	media_rating int NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (client_id) REFERENCES clients(id)
+);
---
+++ config/db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_name, user_email, user_password) VALUES ('Sarah', 'sarahfinck95@gmail.com', 'Boxing1');
+INSERT INTO users (user_name, user_email, user_password) VALUES ('Becky', 'pcgwriter@yahoo.com', 'Webdev1');
+
+INSERT INTO media (media_name, media_type, media_artist, media_location, media_rating) VALUES ('Lord of the Rings', 'movie', 'Peter Jackson', 'Amazon Prime', 5);
+INSERT INTO media (media_name, media_type, media_artist, media_location, media_rating ) VALUES ('Ginny and Georgia', 'tv', 'Anya Adams', 'Netflix', 3);
+INSERT INTO media (media_name, media_type, media_artist, media_location, media_rating ) VALUES ('Cardgian', 'Music', 'Taylor Swift', 'Spotify', 4);
+INSERT INTO media (media_name, media_type, media_artist, media_location, media_rating ) VALUES ('Gone Girl', 'book', 'Gillian Flynn', 'Kindle', 5);
