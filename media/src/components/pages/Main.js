e39f9a9cfa0419c298181be092668397c6683d64
michael swigat <sherlockholmes.swigart92@gmail.com>
2021-05-19 21:14:55 -0500

    THANKS TIM

--- media/src/components/pages/Main.js
+++ media/src/components/pages/Main.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {useAuth0} from '@auth0/auth0-react'
+import React, { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import Button from '@material-ui/core/Button';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Grow from '@material-ui/core/Grow';
@@ -9,81 +9,127 @@ import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
+import { FormControl, FormLabel, Grid, Input } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  paper: {
-    marginRight: theme.spacing(2),
-  },
+	root: {
+		display: 'flex',
+	},
+	paper: {
+		marginRight: theme.spacing(2),
+	},
 }));
 export default function MenuListComposition() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef(null);
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-    setOpen(false);
-  };
-  function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
-      event.preventDefault();
-      setOpen(false);
-    }
-  }
-  // return focus to the button when we transitioned from !open -> open
-  const { loginWithRedirect } = useAuth0();
-  const prevOpen = React.useRef(open);
-  React.useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
-    }
-    prevOpen.current = open;
-  }, [open]);
-  return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <MenuList>
-          <MenuItem>Profile</MenuItem>
-          <MenuItem>My account</MenuItem>
-          <MenuItem><Link href="/SignUp">Sign Up</Link></MenuItem>
-          <MenuItem onClick={() => loginWithRedirect()}>Login</MenuItem>
-          <MenuItem>Logout</MenuItem>
-        </MenuList>
-      </Paper>
-      <div>
-        <Button
-          ref={anchorRef}
-          aria-controls={open ? 'menu-list-grow' : undefined}
-          aria-haspopup="true"
-          onClick={handleToggle}
-        >
-          Toggle Menu Grow
-        </Button>
-        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
-          {({ TransitionProps, placement }) => (
-            <Grow
-              {...TransitionProps}
-              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-            >
-              <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                    <MenuItem onClick={handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={handleClose}>My account</MenuItem>
-                    <MenuItem onClick={handleClose}>Logout</MenuItem>
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
-      </div>
-    </div>
-  );
+	const classes = useStyles();
+	const [open, setOpen] = React.useState(false);
+	const [title, setTitle] = useState();
+	const [author, setAuthor] = useState();
+	const [location, setLocation] = useState();
+	const [rating, setRating] = useState();
+	const [mediaType, setMediaType] = useState();
+	const [mediaArray, setMediaArray] = useState([]);
+	// return focus to the button when we transitioned from !open -> open
+	const { loginWithRedirect } = useAuth0();
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setMediaArray((previousArray) => [
+			...previousArray,
+			{ title, author, location, rating, mediaType },
+		]);
+		setTitle('');
+		setAuthor('');
+		setLocation('');
+		setRating('');
+		setMediaType('');
+	};
+	return (
+		<>
+			<div className={classes.root}>
+				{open && (
+					<Paper className={classes.paper}>
+						<MenuList>
+							<MenuItem>Profile</MenuItem>
+							<MenuItem>My account</MenuItem>
+							<MenuItem>
+								<Link href='/SignUp'>Sign Up</Link>
+							</MenuItem>
+							<MenuItem onClick={() => loginWithRedirect()}>
+								Login
+							</MenuItem>
+							<MenuItem>Logout</MenuItem>
+						</MenuList>
+					</Paper>
+				)}
+			</div>
+			<Grid
+				container
+				direction='row'
+				justify='space-around'
+				alignItems='center'
+				style={{ height: '300px' }}
+			>
+				<FormControl>
+					<FormLabel htmlFor='title'>title</FormLabel>
+					<Input
+						id='title'
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				</FormControl>
+				<FormControl>
+					<FormLabel htmlFor='author'>author</FormLabel>
+					<Input
+						id='author'
+						onChange={(e) => setAuthor(e.target.value)}
+					/>
+				</FormControl>
+				<FormControl>
+					<FormLabel htmlFor='location'>location</FormLabel>
+					<Input
+						id='location'
+						onChange={(e) => setLocation(e.target.value)}
+					/>
+				</FormControl>
+				<FormControl>
+					<FormLabel htmlFor='rating'>rating</FormLabel>
+					<Input
+						id='rating'
+						onChange={(e) => setRating(e.target.value)}
+					/>
+				</FormControl>
+				<FormControl>
+					<FormLabel htmlFor='mediaType'>mediaType</FormLabel>
+					<Input
+						id='mediaType'
+						onChange={(e) => setMediaType(e.target.value)}
+					/>
+				</FormControl>
+			</Grid>
+			<Grid>
+				<Button onClick={handleSubmit}>Submit</Button>
+			</Grid>
+			<div>{title}</div>
+			<div>{author}</div>
+			<div>{location}</div>
+			<div>{rating}</div>
+			<div>{mediaType}</div>
+			{mediaArray.map((media, index) => (
+				<ul key={index}>
+					<li>
+						title {index + 1}: {media.title}
+					</li>
+					<li>
+						author {index + 1}: {media.author}
+					</li>
+					<li>
+						location {index + 1}: {media.location}
+					</li>
+					<li>
+						rating {index + 1}: {media.rating}
+					</li>
+					<li>
+						mediaType {index + 1}: {media.mediaType}
+					</li>
+				</ul>
+			))}
+		</>
+	);
 }
